2a9c6c7f314ad7084252e84a75d355043b84225f
danielaconceicao <daniela.3597@gmail.com>
2024-10-18 06:24:43 +0200

    implementing the logic

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const emails = document.querySelector('.emails');
+/* const emails = document.querySelector('.emails');
 const btn = document.querySelector('.btn');
+ */
 
-
-btn.addEventListener('click', function(){
+/* btn.addEventListener('click', function(){
     axios.get(" https://flynn.boolean.careers/exercises/api/random/mail")
     .then(response => {
       console.log(response);
@@ -13,4 +13,13 @@ btn.addEventListener('click', function(){
     }).catch(err => console.error(err))
 
 
-})
+}) */
+
+const arrNome = [];
+for (let i = 0; i < 10; i++) {
+  const userName = `user${i}`;
+  const userEmail = `${userName}@mante.com`;
+  arrNome.push(userEmail);
+}
+
+console.log(arrNome);
