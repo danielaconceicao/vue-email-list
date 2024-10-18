55cebd512f123c97159289d62b28cc62eb4eede5
danielaconceicao <daniela.3597@gmail.com>
2024-10-18 09:03:31 +0200

    trying to stampe the emails on the page

--- app.js
+++ app.js
@@ -2,33 +2,47 @@ const emails = document.querySelector('.emails');
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', function () {
-  axios.get(" https://flynn.boolean.careers/exercises/api/random/mail")
+  
+  const arrEmails = [];
+
+  for (let i = 0; i < 10; i++) {
+    axios.get(" https://flynn.boolean.careers/exercises/api/random/mail")
     .then(response => {
-      console.log(response);
+      //console.log(response);
+      arrEmails.push(response.data);
+      
+    }).catch(err => console.error(err));
+  }
 
-      const user = response.data.response;
-      const arrNome = [];
+  console.log(arrEmails);
 
-      for (let i = 0; i < 10; i++) {
+  const list = document.createElement('li');
+  list.textContent = arrEmails;
+  emails.appendChild(list)
+  
+});
+
+/* 
+      /* for (let i = 0; i < 10; i++) {
+        user = response.data;
         console.log(user)
-      }
+      }  */
+      
+      //console.log(arrEmails);
+      
+      /* const [user1] = arrEmails
 
-
+      const list = document.createElement('li');
+      list.textContent = user1;
+      emails.appendChild(list); */
 
       /* for (let i = 0; i < 10; i++) {
         const userName = `user${i}`;
         const userEmail = `${userName}@mante.com`;
-        arrNome.push(userEmail);
+        arrEmails.push(userEmail);
       }
-      console.log(arrNome); 
+      console.log(arrEmails); 
 
       const paragraph = document.createElement('p');
-      paragraph.textContent = arrNome;
+      paragraph.textContent = arrEmails;
       emails.insertAdjacentHTML('afterbegin', response); */
-
-    }).catch(err => console.error(err));
-
-
-});
-
-
