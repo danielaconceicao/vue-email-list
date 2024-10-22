a87a924ae0038345fb1e11e81ab82ae87164dde7
danielaconceicao <daniela.3597@gmail.com>
2024-10-22 08:32:12 +0200

    app list modified and finalized

--- app.js
+++ app.js
@@ -1,48 +1,17 @@
-const emails = document.querySelector('.emails');
-const btn = document.querySelector('.btn');
+const emailsEl = document.querySelector('.emails-space');
+const btnEl = document.querySelector('.btn');
 
-btn.addEventListener('click', function () {
-  
-  const arrEmails = [];
+btnEl.addEventListener('click', function () {
 
   for (let i = 0; i < 10; i++) {
-    axios.get(" https://flynn.boolean.careers/exercises/api/random/mail")
-    .then(response => {
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(responses => {
+      const response = responses.data;
       //console.log(response);
-      arrEmails.push(response.data);
-      
+      const list = document.createElement('li');
+      list.textContent = response.response;
+      emailsEl.appendChild(list)
+
     }).catch(err => console.error(err));
   }
-
-  console.log(arrEmails);
-
-  const list = document.createElement('li');
-  list.textContent = arrEmails;
-  emails.appendChild(list)
-  
 });
-
-/* 
-      /* for (let i = 0; i < 10; i++) {
-        user = response.data;
-        console.log(user)
-      }  */
-      
-      //console.log(arrEmails);
-      
-      /* const [user1] = arrEmails
-
-      const list = document.createElement('li');
-      list.textContent = user1;
-      emails.appendChild(list); */
-
-      /* for (let i = 0; i < 10; i++) {
-        const userName = `user${i}`;
-        const userEmail = `${userName}@mante.com`;
-        arrEmails.push(userEmail);
-      }
-      console.log(arrEmails); 
-
-      const paragraph = document.createElement('p');
-      paragraph.textContent = arrEmails;
-      emails.insertAdjacentHTML('afterbegin', response); */
